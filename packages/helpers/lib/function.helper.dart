39125ea1d4aa7114f14513e928a7679c64f9be57
Thibault Zanini <thibaultzanini@gmail.com>
2023-09-28 18:17:36 +0200

    feat: add retry helper

--- packages/helpers/lib/function.helper.dart
+++ packages/helpers/lib/function.helper.dart
@@ -1,3 +1,63 @@
 //ignore_for_file: no-empty-block
+import 'dart:async';
+import 'dart:math';
 
+/// A no-operation function that does nothing.
 void noop() {}
+
+/// Retries a given asynchronous function [task] for a specified
+/// number of [maxAttempts] with an exponential backoff delay.
+///
+/// [task]: The function to retry if it throws an exception.
+/// [maxAttempts]: The maximum number of attempts before throwing an
+///                exception. Default is 2.
+/// [validate]: A function that checks if the returned value from [task]
+///             is valid. If this function returns `false`, the result is
+///             considered invalid and the task is retried.
+/// [initialDelay]: The initial delay before retrying. This delay is
+///                 doubled after every failed attempt. Default is 1 second.
+/// [maxDelay]: The maximum delay between retries. If the exponential backoff
+///             delay exceeds this value, [maxDelay] is used instead.
+///
+/// Returns a [Future<T>] representing the result of the function,
+/// if successful.
+///
+/// Throws an [Exception] if [task] fails after [maxAttempts].
+Future<T> retry<T>({
+  required Future<T> Function() task,
+  int maxAttempts = 2,
+  bool Function(T result)? validate,
+  Duration initialDelay = const Duration(seconds: 1),
+  Duration? maxDelay,
+}) async {
+  for (int attempt = 0; attempt < maxAttempts; attempt++) {
+    try {
+      final T result = await task();
+
+      if (validate == null || validate(result)) {
+        return result;
+      }
+
+      throw Exception('Validation failed for the result.');
+    } catch (e) {
+      if (attempt == maxAttempts - 1) {
+        rethrow; // if it's the last attempt, rethrow the exception
+      }
+
+      /// Calculate delay based on exponential backoff with factor of 2
+      final backoffFactor = pow(2, attempt).round();
+      Duration delay = Duration(
+        milliseconds: (initialDelay.inMilliseconds * backoffFactor),
+      );
+
+      /// Ensure the delay does not exceed the specified [maxDelay].
+      if (maxDelay != null && delay > maxDelay) {
+        delay = maxDelay;
+      }
+
+      await Future.delayed(delay); // wait before the next attempt
+    }
+  }
+
+  throw Exception('Failed after $maxAttempts attempts.');
+}
---
+++ packages/helpers/test/function_helper_test.dart
@@ -0,0 +1,120 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:t_helpers/helpers.dart';
+
+void main() {
+  group('retry function tests', () {
+    test('Successfully returns value without any retries', () async {
+      final result = await retry<String>(
+        task: () async => 'Success!',
+      );
+
+      expect(result, 'Success!');
+    });
+
+    test('Successfully returns value after some retries', () async {
+      int attemptCounter = 0;
+      final result = await retry<String>(
+        task: () async {
+          attemptCounter++;
+
+          if (attemptCounter < 3) {
+            throw Exception('Simulated error.');
+          }
+
+          return 'Success after retries!';
+        },
+        maxAttempts: 5,
+      );
+
+      expect(result, 'Success after retries!');
+      expect(attemptCounter, 3);
+    });
+
+    test('Fails after maxAttempts reached', () async {
+      int attemptCounter = 0;
+
+      final future = retry<String>(
+        task: () async {
+          attemptCounter++;
+          throw Exception('Simulated error.');
+        },
+        maxAttempts: 3,
+      );
+
+      await expectLater(
+        future,
+        throwsA(isA<Exception>()),
+      );
+      expect(attemptCounter, 3);
+    });
+
+    test('Retries when validation fails', () async {
+      int attemptCounter = 0;
+
+      final result = await retry<String>(
+        task: () async {
+          attemptCounter++;
+          return attemptCounter < 3 ? 'Invalid' : 'Valid';
+        },
+        maxAttempts: 5,
+        validate: (res) => res == 'Valid',
+      );
+
+      expect(result, 'Valid');
+      expect(attemptCounter, 3);
+    });
+
+    test('Waits the correct delay between retries', () async {
+      final stopwatch = Stopwatch()..start();
+
+      try {
+        await retry<String>(
+          task: () async {
+            throw Exception('Simulated error.');
+          },
+          maxAttempts: 4,
+          initialDelay: const Duration(seconds: 2),
+        );
+      } catch (_) {
+        // 2 + 4 + 8 seconds
+        expect(
+          stopwatch.elapsed,
+          greaterThanOrEqualTo(const Duration(seconds: 14)),
+        );
+        expect(
+          stopwatch.elapsed,
+          lessThanOrEqualTo(const Duration(seconds: 15)),
+        );
+      } finally {
+        stopwatch.stop();
+      }
+    });
+
+    test('Honors maxDelay for retries', () async {
+      final stopwatch = Stopwatch()..start();
+
+      try {
+        await retry<String>(
+          task: () async {
+            throw Exception('Simulated error.');
+          },
+          maxAttempts: 4,
+          initialDelay: const Duration(seconds: 2),
+          maxDelay: const Duration(seconds: 3),
+        );
+      } catch (_) {
+        //  2 + 3 + 3 seconds
+        expect(
+          stopwatch.elapsed,
+          greaterThanOrEqualTo(const Duration(seconds: 8)),
+        );
+        expect(
+          stopwatch.elapsed,
+          lessThanOrEqualTo(const Duration(seconds: 9)),
+        );
+      } finally {
+        stopwatch.stop();
+      }
+    });
+  });
+}
