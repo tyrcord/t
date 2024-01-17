5bf36e4eecfeb5bf6c02fd42ea79c7cfa1aa5a9a
GitHub Action <actions@github.com>
2024-01-17 18:11:47 +0000

    Update files from codex repository

--- scripts/update_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-# update pubspec.yaml
-python3 scripts/update_pubspec.py
-
-# Upgrade Flutter dependencies
-flutter pub upgrade
